58fdc03bfc280139962fe128f310f31c62652754
emfaisal155-droid <em.faisal155@gmail.com>
2026-03-18 22:38:23 +0300

    Define User class with properties and notes collection

--- backened/Cognilink.core/User.cs
+++ backened/Cognilink.core/User.cs
@@ -1,1 +1,22 @@
+using Cognilink.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognilink.core
+{
+    internal class User
+    {
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public ICollection<Note> Notes { get; set; } = new List<Note>();
+}
 

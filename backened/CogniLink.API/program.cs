ce3397031aadfa4860dc59e4ba9a9adc67e3c04b
emfaisal155-droid <em.faisal155@gmail.com>
2026-03-18 22:09:22 +0300

    Update program.cs

--- backened/CogniLink.API/program.cs
+++ backened/CogniLink.API/program.cs
@@ -1,1 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Cognilink.infrastructure;
+using Cognilink.infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<INoteRepository, NoteRepository>();  //This allows Ammara to ask for INoteRepository in her Controllers, and the system will automatically provide the database logic you wrote.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
 

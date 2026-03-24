985ed3e18207fb3c6be923ec9ee48dbc245f80a1
emfaisal155-droid <em.faisal155@gmail.com>
2026-03-24 20:49:09 +0300

    Add seed data for test user and sample notes

--- database/seed.sql
+++ database/seed.sql
@@ -1,8 +1,6 @@
--- Create a test user (Note: Id will be 1)
 INSERT INTO [Users] ([Username], [PasswordHash]) 
 VALUES ('Tester', 'AQAAAAEAACcQAAAAE...');
 
--- Create sample notes for that user
 INSERT INTO [Notes] ([Title], [Content], [UserId]) 
 VALUES ('Iteration 1 Tasks', 'Setup GitHub and Database Schema.', 1);
 

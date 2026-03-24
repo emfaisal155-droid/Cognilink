c4269f3c4fc649907f3ddb25e840a355615758ff
emfaisal155-droid <em.faisal155@gmail.com>
2026-03-24 20:48:51 +0300

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -1,2 +1,12 @@
-INSERT INTO Users (Username, PasswordHash) VALUES ('testuser', 'password123');
-INSERT INTO Notes (Title, Content, UserId) VALUES ('Meeting Note', 'Discuss project architecture', 1);
+-- Create a test user (Note: Id will be 1)
+INSERT INTO [Users] ([Username], [PasswordHash]) 
+VALUES ('Tester', 'AQAAAAEAACcQAAAAE...');
+
+-- Create sample notes for that user
+INSERT INTO [Notes] ([Title], [Content], [UserId]) 
+VALUES ('Iteration 1 Tasks', 'Setup GitHub and Database Schema.', 1);
+
+INSERT INTO [Notes] ([Title], [Content], [UserId]) 
+VALUES ('Grocery List', 'Milk, Bread, and Coffee.', 1);
+
+GO

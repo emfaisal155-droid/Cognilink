fd10cd1a33a59c0a05cfd5bebe3468e7034a55a5
Eman Faisal <em.faisal155@gmail.com>
2026-04-11 16:25:21 +0500

    Add files via upload

---
+++ backened/Cognilink.core/Concept.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cognilink.core
+{
+    public class Concept
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Frequency { get; set; }
+        public int NoteId { get; set; }
+        public int UserId { get; set; }
+
+        public Note Note { get; set; } = null!;
+
+        public ICollection<ConceptRelationship> SourceRelationships { get; set; }
+            = new List<ConceptRelationship>();
+        public ICollection<ConceptRelationship> TargetRelationships { get; set; }
+            = new List<ConceptRelationship>();
+    }
+}
---
+++ backened/Cognilink.core/ConceptRelationship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognilink.core
+{
+    public class ConceptRelationship
+    {
+        public int Id { get; set; }
+        public int SourceConceptId { get; set; }
+        public int TargetConceptId { get; set; }
+        public double SimilarityScore { get; set; }
+        public int UserId { get; set; }
+
+        public Concept SourceConcept { get; set; } = null!;
+        public Concept TargetConcept { get; set; } = null!;
+    }
+}

766b95ac95e0302629f8f718faffada7c75593bc
Eman Faisal <em.faisal155@gmail.com>
2026-04-11 16:43:18 +0500

    Update AppDbContext.cs

--- backened/Cognilink.infrastructure/AppDbContext.cs
+++ backened/Cognilink.infrastructure/AppDbContext.cs
@@ -15,6 +15,27 @@ namespace Cognilink.infrastructure
 
         public DbSet<User> Users { get; set; }
         public DbSet<Note> Notes { get; set; }
+
+        //iteration2
+        public DbSet<Concept> Concepts { get; set; }
+        public DbSet<ConceptRelationship> ConceptRelationships { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        
+            modelBuilder.Entity<ConceptRelationship>()
+                .HasOne(r => r.SourceConcept)
+                .WithMany(c => c.SourceRelationships)
+                .HasForeignKey(r => r.SourceConceptId)
+                .OnDelete(DeleteBehavior.Restrict);
+        
+            modelBuilder.Entity<ConceptRelationship>()
+                .HasOne(r => r.TargetConcept)
+                .WithMany(c => c.TargetRelationships)
+                .HasForeignKey(r => r.TargetConceptId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
 

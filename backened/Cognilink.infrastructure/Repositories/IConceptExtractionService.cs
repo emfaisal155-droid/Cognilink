7e36c09edfe06995d857ed2a6e12ade2d5dc67be
Eman Faisal <em.faisal155@gmail.com>
2026-04-11 16:37:19 +0500

    Add files via upload

---
+++ backened/Cognilink.infrastructure/Repositories/IConceptExtractionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognilink.infrastructure.Repositories
+{
+    public interface IConceptExtractionService
+    {
+        // Sub-story 1.1 + 1.3: parse text, extract keywords, filter stop words
+        Task<List<string>> ExtractConceptsAsync(string noteText, int noteId);
+
+        // Sub-story 4.1 + 4.2: re-run extraction when note is updated/deleted
+        Task UpdateConceptsAsync(int noteId, string updatedText);
+    }
+}
---
+++ backened/Cognilink.infrastructure/Repositories/IRelationshipDetectionService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognilink.infrastructure.Repositories
+{
+    public interface IRelationshipDetectionService
+    {
+        // Sub-story 2.1: detect co-occurrence between concepts
+        Task<List<(string ConceptA, string ConceptB, double Score)>>
+            DetectRelationshipsAsync(List<string> concepts);
+
+        // Sub-story 2.2: calculate similarity score between two concepts
+        Task<double> CalculateSimilarityAsync(string conceptA, string conceptB);
+
+        // Sub-story 2.3: fetch all relationships for a specific user
+        Task<List<(string ConceptA, string ConceptB, double Score)>>
+            GetRelationshipsByUserAsync(int userId);
+
+    }
+}

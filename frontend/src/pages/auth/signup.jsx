a77adfa35e2e2569c7da0bb7a7d0126edd298640
farwa-naqvi <farwa5464@gmail.com>
2026-04-13 20:48:57 +0500

    Update signup.jsx

--- frontend/src/pages/auth/signup.jsx
+++ frontend/src/pages/auth/signup.jsx
@@ -26,7 +26,7 @@ export default function Signup() {
 
     try {
       // 2. Send data to the backend
-      const response = await fetch('http://localhost:7174/api/account/register', {
+      const response = await fetch('https://localhost:7174/api/account/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

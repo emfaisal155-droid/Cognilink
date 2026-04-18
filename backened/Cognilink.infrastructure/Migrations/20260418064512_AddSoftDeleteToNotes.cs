44c68a53a4e647900a4d69bfd72a5b611343a37a
farwa-naqvi <farwa5464@gmail.com>
2026-04-18 13:37:28 +0500

    Add files via upload

---
+++ backened/Cognilink.infrastructure/Migrations/20260418064512_AddSoftDeleteToNotes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cognilink.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeleteToNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Notes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Notes");
+        }
+    }
+}
